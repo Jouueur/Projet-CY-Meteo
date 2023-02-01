342b6a4d61f63d53f16c793ed34d620480552f25
Oazin <78148154+Oazin@users.noreply.github.com>
2023-02-01 23:03:52 +0100

    Update avl.h

--- avl.h
+++ avl.h
@@ -1,5 +1,13 @@
+//
+// Created by Marius on 14/12/22.
+//
+
 #include <stdio.h>
 #include <stdlib.h>
+
+#ifndef AVL_H
+#define AVL_H
+
 
 typedef struct AVL{
     int elt,eq;
@@ -7,3 +15,12 @@ typedef struct AVL{
 }AVL;
 
 typedef AVL *pavl;
+
+pavl rotdroite(pavl a);
+pavl rotgauche(pavl a);
+pavl doubleg(pavl a);
+pavl doubled(pavl a);
+pavl equilibrer(pavl a);
+pavl insertAVL(pavl a, int e, int* h);
+
+#endif //AVL_H
