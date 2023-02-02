dc4b46b2a488728096003e2bf1925a0a93f4038d
Jouueur <95230708+Jouueur@users.noreply.github.com>
2023-02-02 20:00:54 +0100

    Update abr.c

--- abr.c
+++ abr.c
@@ -1,129 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "avl.h"
+#include "abr.h"
 
-
-pavl creer(Station x){
-    pavl c = malloc(sizeof(pavl)*6);
-    c->elt.codes = x.codes;
-    c->elt.avg = x.avg;
-    c->elt.min = x.min;
-    c->elt.max = x.max;
-    c->gauche = NULL;
+pabr creerABR(struct Station a){
+    pabr c = malloc(sizeof(pabr)*6);
+    if(c == NULL) exit(1);
+    c->elt = a;
     c->droit = NULL;
-    c->eq = 0;
+    c->gauche = NULL;  
+
     return c;
 }
 
 
-int max(int a, int b){
-    if(a>b)return a;
-    else return b;
-}
-
-int min(int a, int b){
-    if(a<b)return a;
-    else return b;
-}
-
-pavl rotdroite(pavl A){
-    pavl B; int a,b;
-    B = A->gauche;
-    a = A->eq; b = B->eq;
-    A->gauche = B->droit; B->droit = A; /* rotation */
-    A->eq = a-min(b,0)+1;
-    B->eq = max(max(a+2,a+b+2),b+1);
-    return B;
-}
-
-pavl rotgauche(pavl A){
-    pavl B; int a,b;
-    B = A->droit;
-    a = A->eq; b = B->eq;
-    A->droit = B->gauche; B->gauche = A; /* rotation */
-    A->eq = a-max(b,0)-1;
-    B->eq = min(min(a-2,a+b-2),b-1);
-    return B;
-}
-
-pavl doubleg(pavl a){
-    a->droit = rotdroite(a->droit);
-    return rotgauche(a);
-}
-
-pavl doubled(pavl a){          
-    a->gauche = rotgauche(a->gauche);
-    return rotdroite(a);
-}
-
-pavl equilibrer(pavl a){
-    if(a->eq >= 2){
-        if(a->droit->eq >= 0) return rotgauche(a);
-        else return doubleg(a);
+pabr insertABR(pabr a, struct Station st){
+    if(a == NULL){
+        a=creerABR(st);
     }
-    else if(a->eq <= -2){
-        if(a->gauche->eq <= 0) return rotdroite(a);
-        else return doubled(a);
+    else if(a->elt.codes > st.codes){
+        a->gauche = insertABR(a->gauche,st);
     }
-    return NULL;
-}
-
-
-
-pavl insertAVL(pavl a, Station st, int* h){
-    if(a==NULL){
-        *h=1;
-        return creer(st);
-    }
-    else if(st.codes < a->elt.codes){
-        a->gauche = insertAVL(a->gauche,st,h);
-        *h = -*h;
-    }
-    else if(st.codes > a->elt.codes)a->gauche = insertAVL(a->droit,st,h);
-    else {
-        *h=0;
-        return a;
-    }
-    if(*h != 0){
-        a->eq = a->eq + *h;
-        a = equilibrer(a);
-        if(a->eq == 0)*h = 0;
-        else *h = 1;
+    else if(a->elt.codes < st.codes){
+        a->droit = insertABR(a->droit,st);
     }
     return a;
 }
 
-pavl duplicateAVL(pavl a, Station val){
-    int h = 0;
+void duplicateABR(pabr a, struct Station val){
+
     if (a == NULL){
-        return insertAVL(a,val,&h);
+        a = insertABR(a,val);
     }
-        
+
     else if (a->elt.codes == val.codes){
         if(a->elt.min > val.min && val.min != 0.000000) a->elt.min = val.min;
         if(a->elt.max < val.max && val.max != 0.000000) a->elt.max = val.max;
 
         if(a->elt.min > val.avg ) a->elt.min = val.avg;
         if(a->elt.max < val.avg ) a->elt.max = val.avg;
-        
-        // moyenne challah     
-    } 
-       
+
+        // moyenne challah
+
+    }
+
+
     else if (a->elt.codes > val.codes)
-        return insertAVL(duplicateAVL(a->gauche,val),a->elt,&h);
+        duplicateABR (a->gauche,val);
     else if (a->elt.codes < val.codes)
-        return insertAVL(duplicateAVL(a->droit,val),a->elt,&h);
+        duplicateABR (a->droit,val);
 }
 
 
-void infixeAVL(pavl a, FILE* fp){
-    if (a != NULL){
-        infixeAVL(a->gauche,fp);
+
+void infixeABR(pabr a, FILE* fp){
+    if (a == NULL){
+        infixeABR(a->gauche,fp);
         fprintf(fp, "%d, %f, %f, %f\n", a->elt.codes, a->elt.avg, a->elt.min, a->elt.max);
-        infixeAVL(a->droit,fp);
+        infixeABR(a->droit,fp);
     }
 
 }
+
+
+
 
 
 
