d1b4e75bd8b17b65175c0588567df176446029f4
Jouueur <95230708+Jouueur@users.noreply.github.com>
2023-02-03 12:32:58 +0100

    Update sender.sh

--- sender.sh
+++ sender.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-region=$1
-temp_option=$2
-file=$3
+
+temp_option=$1
+file=$2
+sort=$3
+region=$4
 
 
+echo $1 $2 $3 $4
+
+echo $output_file
 # Set values of choosed columns based on the option
 if [ "$temp_option" == "wind" ]; then
   param1=4
@@ -12,20 +17,21 @@ if [ "$temp_option" == "wind" ]; then
   param3=10
   param4=11
 elif [ "$temp_option" == "height" ]; then
-  param1=15
-  param2=10
-  param3=11
-  param4=20
+  param1=1
+  param2=15
+  param3=10
+  param4=11
 elif [ "$temp_option" == "moisture" ]; then
-  param1=6
-  param2=10
-  param3=11
-  param4=20
+  param1=1
+  param2=6
+  param3=10
+  param4=11
 elif [ "$temp_option" == "t1" ]; then
   param1=1  
   param2=12
   param3=13
   param4=14
+  temp_option=1
 elif [ "$temp_option" == "t2" ]; then
   param1=2
   param2=12
@@ -38,9 +44,10 @@ elif [ "$temp_option" == "t3" ]; then
   param4=1
 elif [ "$temp_option" == "p1" ]; then
   param1=1
-  param2=3
-  param3=7
+  param2=7
+  param3=3
   param4=8
+  temp_option=4
 elif [ "$temp_option" == "p2" ]; then
   param1=1
   param2=3
@@ -60,35 +67,38 @@ fi
 # Extract rows from meteodata.csv based on region and param values, and write to results.csv
 
 if [ "$region" == "France" ]; then
-  awk -F, -v p1=$param1 -v p2=$param2 -v p3=$param3 -v p4=$param4 '$16 < "96000" && $16 > "1000" {print $p1 "," $p2 "," $p3 "," $p4}' $file > results.csv
+  awk -F, -v p1=$param1 -v p2=$param2 -v p3=$param3 -v p4=$param4 '$16 < "96000" && $16 > "1000" {print $p1 "," $p2 "," $p3 "," $p4}' $file > filtred.csv
 
 elif [ "$region" == "Guyanefrancaise" ]; then
   # Extract rows from meteodata.csv for Guyane française
-  awk -F, -v p1=$param1 -v p2=$param2 -v p3=$param3 -v p4=$param4 '$16 >= 97301 && $16 <= 97362 {print $p1 "," $p2 "," $p3 "," $p4}' $file > results.csv
+  awk -F, -v p1=$param1 -v p2=$param2 -v p3=$param3 -v p4=$param4 '$16 >= 97301 && $16 <= 97362 {print $p1 "," $p2 "," $p3 "," $p4}' $file > filtred.csv
 
 elif [ "$region" == "Spm" ]; then
   # Extract rows from meteodata.csv for Saint-Pierre et Miquelon
-  awk -F, -v p1=$param1 -v p2=$param2 -v p3=$param3 -v p4=$param4 '$10 == 46.766333 && $11 == -56.179167 {print $p1 "," $p2 "," $p3 "," $p4}' $file > results.csv
+  awk -F, -v p1=$param1 -v p2=$param2 -v p3=$param3 -v p4=$param4 '$10 == 46.766333 && $11 == -56.179167 {print $p1 "," $p2 "," $p3 "," $p4}' $file > filtred.csv
 
 elif [ "$region" == "Antilles" ]; then
   # Extract rows from meteodata.csv for Antilles
-  awk -F, -v p1=$param1 -v p2=$param2 -v p3=$param3 -v p4=$param4 '$16 >= 97201 && $16 <= 97232 {print $p1 "," $p2 "," $p3 "," $p4}' $file > results.csv
+  awk -F, -v p1=$param1 -v p2=$param2 -v p3=$param3 -v p4=$param4 '$16 >= 97201 && $16 <= 97232 {print $p1 "," $p2 "," $p3 "," $p4}' $file > filtred.csv
 
 elif [ "$region" == "Oceanindien" ]; then
   # Extract rows from meteodata.csv for Océan indien
-  awk -F, -v p1=$param1 -v p2=$param2 -v p3=$param3 -v p4=$param4 '$10 >= -49.352333 && $10 <= -15.887667 && $11 <= 77.569167 && $11 >= 40.340667 {print $p1 "," $p2 "," $p3 "," $p4}' $file > results.csv
+  awk -F, -v p1=$param1 -v p2=$param2 -v p3=$param3 -v p4=$param4 '$10 >= -49.352333 && $10 <= -15.887667 && $11 <= 77.569167 && $11 >= 40.340667 {print $p1 "," $p2 "," $p3 "," $p4}' $file > filtred.csv
 
 elif [ "$region" == "Antartique" ]; then
   # Extract rows from meteodata.csv for Antartique
-  awk -F, -v p1=$param1 -v p2=$param2 -v p3=$param3  -v p4=$param4 '$10 == -66.663167 && $11 == 140.001 {print $p1 "," $p2 "," $p3 "," $p4}' $file > results.csv
+  awk -F, -v p1=$param1 -v p2=$param2 -v p3=$param3  -v p4=$param4 '$10 == -66.663167 && $11 == 140.001 {print $p1 "," $p2 "," $p3 "," $p4}' $file > filtred.csv
 
-elif [ "$region" == "" ]; then
+elif [ "$region" == "ALL" ]; then
   # Extract rows from meteodata.csv for everywhere
-  awk -F, -v p1=$param1 -v p2=$param2 -v p3=$param3  -v p4=$param4 '{print $p1 "," $p2 "," $p3 "," $p4}' $file > results.csv
+  awk -F, -v p1=$param1 -v p2=$param2 -v p3=$param3  -v p4=$param4 '{print $p1 "," $p2 "," $p3 "," $p4}' $file > filtred.csv
 fi
 
-./moist.gp
 
-#gcc -o mainc mainc.c
-#./mainc
+make
+./main-project $sort filtred.csv $temp_option $output_file 
+
+
+
+
 
