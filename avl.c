5b5875fb458b7e634a1c6cbcf115f26c1cfe5b43
Jouueur <95230708+Jouueur@users.noreply.github.com>
2023-02-04 01:02:55 +0100

    Update avl.c

--- avl.c
+++ avl.c
@@ -1,111 +1,152 @@
-#include <stdio.h>
-#include <stdlib.h>
+
 #include "avl.h"
 
 
-pavl creer(Station x){
-    pavl c = malloc(sizeof(pavl)*6);
-    c->elt.codes = x.codes;
-    c->elt.avg = x.avg;
-    c->elt.min = x.min;
-    c->elt.max = x.max;
-    c->left = NULL;
-    c->right = NULL;
-    c->eq = 0;
-    return c;
+int height(pavl N){
+    if (N == NULL)
+        return 0;
+    return N->height;
 }
-
 
 int max(int a, int b){
-    if(a>b)return a;
-    else return b;
+    return (a > b)? a : b;
 }
 
-int min(int a, int b){
-    if(a<b)return a;
-    else return b;
+pavl newNode(Station st){
+    pavl temp = (pavl) malloc(sizeof(pavl)*6);
+    temp->st = st;
+    temp->left = NULL;
+    temp->right = NULL;
+    temp->height = 1;
+    return temp;
 }
 
-pavl rotrighte(pavl A){
-    pavl B; int a,b;
-    B = A->left;
-    a = A->eq; b = B->eq;
-    A->left = B->right; B->right = A; /* rotation */
-    A->eq = a-min(b,0)+1;
-    B->eq = max(max(a+2,a+b+2),b+1);
-    return B;
+pavl rightRotate(pavl y){
+    pavl x = y->left;
+    pavl T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = max(height(y->left), height(y->right)) + 1;
+    x->height = max(height(x->left),height(x->right)) + 1;
+
+    return x;
 }
 
-pavl rotleft(pavl A){
-    pavl B; int a,b;
-    B = A->right;
-    a = A->eq; b = B->eq;
-    A->right = B->left; B->left = A; /* rotation */
-    A->eq = a-max(b,0)-1;
-    B->eq = min(min(a-2,a+b-2),b-1);
-    return B;
+// A utility function to left rotate subtree rooted with x
+
+pavl leftRotate(pavl x){
+    pavl y = x->right;
+    pavl T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = max(height(x->left), height(x->right)) + 1;
+    y->height = max(height(y->left), height(y->right)) + 1;
+
+    return y;
 }
 
-pavl doubleg(pavl a){
-    a->right = rotrighte(a->right);
-    return rotleft(a);
+// Get Balance factor of node N
+int getBalance(pavl N){
+    if (N == NULL)
+        return 0;
+    return height(N->left) - height(N->right);
 }
 
-pavl doubled(pavl a){
-    a->left = rotleft(a->left);
-    return rotrighte(a);
-}
+// Recursive function to insert a key in the subtree rooted
+// with node and returns the new root of the subtree.
+pavl insertt1AVL(pavl a, Station st){
+    if (a == NULL)
+        return(newNode(st));
+    else if (a->st.codes == st.codes){
+        if(a->st.min > st.min && st.min != 0.000000) a->st.min = st.min;
+        if(a->st.max < st.max && st.max != 0.000000) a->st.max = st.max;
 
-pavl equilibrer(pavl a){
-    if(a->eq >= 2){
-        if(a->right->eq >= 0) return rotleft(a);
-        else return doubleg(a);
-    }
-    else if(a->eq <= -2){
-        if(a->left->eq <= 0) return rotrighte(a);
-        else return doubled(a);
-    }
-    return NULL;
-}
+        if(a->st.min > st.avg ) a->st.min = st.avg;
+        if(a->st.max < st.avg ) a->st.max = st.avg;
 
+        a->st.avg = (a->st.avg + st.avg ) /2 -0.5;
 
-
-pavl insertAVL(pavl a, Station st, int* h){
-    if(a==NULL){
-        *h=1;
-        return creer(st);
-    } else if (a->elt.codes == st.codes){
-        if(a->elt.min > st.min && st.min != 0.000000) a->elt.min = st.min;
-        if(a->elt.max < st.max && st.max != 0.000000) a->elt.max = st.max;
-
-        if(a->elt.min > st.avg ) a->elt.min = st.avg;
-        if(a->elt.max < st.avg ) a->elt.max = st.avg;
-
-        // moyenne challah
-    }
-
-    else if(st.codes < a->elt.codes){
-        a->left = insertAVL(a->left,st,h);
-        *h = -*h;
-    }
-    else if(st.codes > a->elt.codes)a->left = insertAVL(a->right,st,h);
-    else {
-        *h=0;
+    } else if (st.codes < a->st.codes)
+        a->left = insertt1AVL(a->left, st);
+    else if (st.codes > a->st.codes)
+        a->right = insertt1AVL(a->right, st);
+    else
         return a;
+
+    a->height = 1 + max(height(a->left), height(a->right));
+
+    int balance = getBalance(a);
+
+    if (balance > 1 && st.codes < a->left->st.codes)
+        return rightRotate(a);
+
+    if (balance < -1 && st.codes > a->right->st.codes)
+        return leftRotate(a);
+
+    if (balance > 1 && st.codes > a->left->st.codes)
+    {
+        a->left = leftRotate(a->left);
+        return rightRotate(a);
     }
-    if(*h != 0){
-        a->eq = a->eq + *h;
-        a = equilibrer(a);
-        if(a->eq == 0)*h = 0;
-        else *h = 1;
+
+    if (balance < -1 && st.codes < a->right->st.codes)
+    {
+        a->right = rightRotate(a->right);
+        return leftRotate(a);
     }
+
+    return a;
+}
+
+pavl insertp1AVL(pavl a, Station st){
+    if (a == NULL)
+        return(newNode(st));
+    else if (a->st.codes == st.codes){
+        if (a->st.min > st.avg && st.avg != 0.000000) a->st.min = st.avg;
+        if (a->st.max < st.avg && st.avg != 0.000000) a->st.max = st.avg;
+
+        a->st.avg = (a->st.avg + st.avg ) /2 -0.5;
+
+    } else if (st.codes < a->st.codes)
+        a->left = insertp1AVL(a->left, st);
+    else if (st.codes > a->st.codes)
+        a->right = insertp1AVL(a->right, st);
+    else
+        return a;
+
+    a->height = 1 + max(height(a->left), height(a->right));
+
+    int balance = getBalance(a);
+
+    if (balance > 1 && st.codes < a->left->st.codes)
+        return rightRotate(a);
+
+    if (balance < -1 && st.codes > a->right->st.codes)
+        return leftRotate(a);
+
+    if (balance > 1 && st.codes > a->left->st.codes)
+    {
+        a->left = leftRotate(a->left);
+        return rightRotate(a);
+    }
+
+    if (balance < -1 && st.codes < a->right->st.codes)
+    {
+        a->right = rightRotate(a->right);
+        return leftRotate(a);
+    }
+
     return a;
 }
 
 void infixeAVL(pavl a, FILE* fp){
     if (a != NULL){
         infixeAVL(a->left,fp);
-        fprintf(fp, "%d, %f, %f, %f\n", a->elt.codes, a->elt.avg, a->elt.min, a->elt.max);
+        fprintf(fp, "%d, %f, %f, %f\n", a->st.codes, a->st.avg, a->st.min, a->st.max);
         infixeAVL(a->right,fp);
     }
 
